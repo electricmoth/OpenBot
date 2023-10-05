1b90752678ac847ae5206e4aa9c814ebd753ed23
electricmoth <74592516+electricmoth@users.noreply.github.com>
2023-10-04 21:23:20 -0700

    add run_openbot.sh

---
+++ run_openbot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# env vars for openbot
+
+
+PASSWD=`pass meetup.com`
+EMAIL=definitely_reAL_email@example.com
+EVENT_URL=https://www.meetup.com/event-url
+
+/path/to/openbot/venv/bin/python /path/to/run_openbot.sh
